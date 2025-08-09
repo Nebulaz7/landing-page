a4d1fc46d437edcf431549778f22d3c6d6d94aa5
Nebulaz7 <jp0712002@gmail.com>
2025-08-09 01:00:42 +0100

    Add Feature component and integrate it into Home page for enhanced functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Feature from "./components/Feature";
 
 export default function Home() {
   return (
     <div className="grid-background">
       <Navbar />
       <Hero />
+      <Feature />
     </div>
   );
 }
