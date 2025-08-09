2293ba2d3682713a29afb489555456d43c3f2ed4
Nebulaz7 <jp0712002@gmail.com>
2025-08-10 00:05:05 +0100

    Refactor Feature component and update CSS variables for consistency

--- app/components/Feature.tsx
+++ app/components/Feature.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { SparklesIcon } from "lucide-react";
 
 const Feature = () => {
   return (
-    <div>
-      {/* <h2 className="text-3xl font-bold text-center my-8">Feature Section</h2>
-      <p className="text-center text-gray-600">
-        This is where you can highlight the features of your application.
-      </p> */}
-    </div>
+    <section id="features" aria-labelledby="features-label" className="py-12">
+      {/* <div className="flex justify-center">
+        <span className="inline-flex rounded-full bg-gradient-to-r from-[#9810fa] via-[#7182ff] to-[#3cff52] p-[2px]">
+          <span
+            id="features-label"
+            className="inline-flex items-center inset-3 shadow-purple-500 shadow-2xl gap-2 rounded-full px-5 py-1.5 text-sm font-medium text-purple-100 bg-[var(--background)]"
+          >
+            <SparklesIcon className="w-5 h-5 text-[#9810fa]" />
+            Features
+          </span>
+        </span>
+      </div> */}
+      <div className="flex">
+        <div className="px-3">
+          <h1 className="text-3xl lg:text-4xl font-light hero-font leading-tight mb-6">
+            Natural Voice Synthesis
+          </h1>
+          <p>
+            Advanced neural networks create voices that capture human emotion,
+            tone, and personality with unprecedented realism.
+          </p>
+        </div>
+        <div>{/* illustation */}</div>
+      </div>
+    </section>
   );
 };
 
